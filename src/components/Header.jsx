b1bac54ce8469151e4f4a5c7549009537b914753
AnnaSusak <anna.i.susak@gmail.com>
2022-11-02 16:59:21 +0300

    firstcommit

---
+++ src/components/Header.jsx
@@ -0,0 +1,72 @@
+import { FlutterDash } from '@mui/icons-material';
+import { AppBar, IconButton, Toolbar, Typography, Box, Menu, MenuItem} from '@mui/material';
+import * as React from 'react';
+import Badge from '@mui/material/Badge';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
+const Header = () => {
+    return (
+    <AppBar position="fixed">
+
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+          >
+            MUI
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="error">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={17} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+    </AppBar>
+    )
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+const Profile = () => {
+  return (
+    <div className='Profile'><img src='https://answit.com/wp-content/uploads/2017/01/full-hd.jpg'></img>
+      <div>ava+description</div>
+      <div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div></div>);
+}
+export default Profile;
